352cac4f2e649f9ebaf52a8e7a439f2c22e21188
OkkarMin <omin001@e.ntu.edu.sg>
2024-01-01 11:19:55 +0800

    feat: add deploy to github pages action

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cww-sg.github.io',
   integrations: [
     starlight({
       title: 'My Docs',
